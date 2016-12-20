59abdb1f1697dfd6d0ada0099649e7f638b030af
Anders Rosen <anders.rosen@inlumino.se>
2016-12-20 21:24:54 +0100

    First solution to day 13 completed

--- mrinlumino-python/13.py
+++ mrinlumino-python/13.py
@@ -19,77 +19,93 @@ print '********** Challenge 1 ************'
 
 maze = []
 
-maxSize = 48
+# Create a big enough maze to hold the solution
+maxSize = 50
 
-# Create an open maze of size 100x100
+# Create an open maze filled with spaces
 for y in range(maxSize):
-	row = ''
+	row = []
 	for x in range(maxSize):
-		str += ' '
-	maze.append(str)
+		row.append(' ')
+	maze.append(row)
 
-
+# Build the labyrinth
 for y in range(maxSize):
 	for x in range(maxSize):
 		baseValue = x*x + 3*x + 2*x*y + y + y*y + puzzleInput
 		binaryString = bin(baseValue)[2:]
 		if binaryString.count('1') % 2 == 1:
-			maze[y] = maze[y][:x] + '#' + maze[y][x+1:]
+			maze[y][x] = '#'
+
+# Function that marks open positions around a specified coordinate with weighted values
+# Calls itself recursively until every available position is weighted
+def markNeighbors(posY,posX,value):
+	if posX > 0:
+		if maze[posY][posX-1] == ' ': 
+			maze[posY][posX-1] = value
+			markNeighbors(posY,posX-1,value+1)
+	if posX < maxSize-1:
+		if maze[posY][posX+1] == ' ': 
+			maze[posY][posX+1] = value
+			markNeighbors(posY,posX+1,value+1)
+	if posY > 0:
+		if maze[posY-1][posX] == ' ': 
+			maze[posY-1][posX] = value
+			markNeighbors(posY-1,posX,value+1)
+	if posY < maxSize-1:
+		if maze[posY+1][posX] == ' ': 
+			maze[posY+1][posX] = value
+			markNeighbors(posY+1,posX,value+1)
+
+# A function that finds the position with the lowest weight value and returns its position
+def findNextPosition(curPos):
+	y = curPos[0]
+	x = curPos[1]
+	bestX = x
+	bestY = y
+	minVal = 1000
+	if y>0:
+		if maze[y-1][x] < minVal:
+			bestY = y-1
+			bestX = x
+			minVal = maze[y-1][x] 
+			#print minVal
+	if y<maxSize-1:
+		if maze[y+1][x] < minVal:
+			bestY = y+1
+			bestX = x
+			minVal = maze[y+1][x] 
+	if x>0:
+		if maze[y][x-1] < minVal:
+			bestX = x-1
+			bestY = y
+			minVal = maze[y][x-1] 
+	if x<maxSize-1:
+		if maze[y][x+1] < minVal:
+			bestX = x+1
+			bestY = y
+			minVal = maze[y][x+1] 
+	return [bestY,bestX]
 
 
-x = 1
-y = 1
-maze[1] = maze[1][:1] + 'o' + maze[1][2:]
-currentDirection = 'D'
-while x != 31 and y != 39:
+# Starting position
+maze[1][1] = 0
 
-	for s in maze:
-		print s
+# Call the recursive function to fill the maze with weighted values
+markNeighbors(1,1,1)
 
-	foundMyWay = 0
+# Start from the target position and walk backwards using the lowest available value path
+currentPosition = [39,31]
+numberOfSteps = 0
 
-	if currentDirection == 'D': tryDirection = ['R','D','L','U']
-	if currentDirection == 'R': tryDirection = ['U','R','D','L']
-	if currentDirection == 'U': tryDirection = ['L','U','R','D']
-	if currentDirection == 'L': tryDirection = ['D','L','U','R']
+# Walk until were back at the starting position
+while currentPosition != [1,1]:
+	# Increase the number of steps
+	numberOfSteps += 1
+	currentPosition = findNextPosition(currentPosition)
 
-	orgX = x
-	orgY = y
-
-	for dir in range(4):
-		if foundMyWay == 0: currentDirection = tryDirection[dir]
-
-		# Test left
-		if currentDirection == 'L' and foundMyWay == 0 and x > 0:
-			if maze[y][x-1] == ' ' or maze[y][x-1] == 'o':
-				foundMyWay = 1
-				x -= 1
+print 'Challenge 1: Number of steps through the maze was: %s' % numberOfSteps
 
 
-		# Test up
-		if currentDirection == 'U'  and foundMyWay == 0 and y > 0:
-			if maze[y-1][x] == ' ' or maze[y-1][x] == 'o':
-				foundMyWay = 1
-				y -= 1
-
-		# Test right
-		if currentDirection == 'R' and foundMyWay == 0 and x < maxSize:
-			if maze[y][x+1] == ' ' or maze[y][x+1] == 'o':
-				foundMyWay = 1
-				x += 1
 
 
-		# Test down
-		if currentDirection == 'D'  and foundMyWay == 0 and y < maxSize:
-			if maze[y+1][x] == ' ' or maze[y+1][x] == 'o':
-				foundMyWay = 1
-				y += 1
-
-	print ('foundmywy=%s and walked to y:x %s:%s') % (foundMyWay, y,x)
-	if maze[y][x] == ' ':
-		maze[y] = maze[y][:x] + 'o' + maze[y][x+1:]
-	else:
-		maze[orgY] = maze[orgY][:orgX] + '.' + maze[orgY][orgX+1:]
-
-
-	choice = raw_input("> ")
