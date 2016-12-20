d60cc7a48e16b1bf89c475fdd2c48ea2bd7d3ca3
Anders Rosen <anders.rosen@inlumino.se>
2016-12-20 09:36:56 +0100

    Both solutions done for day 19

--- mrinlumino-python/19.py
+++ mrinlumino-python/19.py
@@ -14,38 +14,8 @@ print ''
 
 
 
-# # ****************************************** challenge 1 ****************************************** 
-# print '********** Challenge 1 ************'
-
-# NoOfElves = 3005290
-
-
-# # Create an array corresponding to the elves with one present each
-# elves = []
-# for createCounter in range(NoOfElves): elves.append(createCounter+1)
-
-
-# elfCounter = 0
-# # Do the present shift and exclusion of elves until there is only one elf left
-# while len(elves) > 1:
-# 	if len(elves) % 10000 == 0: print len(elves) / 10000
-
-# 	# Are we currently at the last elf in the ring, then remove the first one, otherwise just remove the next in line
-# 	if elfCounter == len(elves)-1:
-# 		elves.pop(0)
-# 	else:
-# 		elves.pop(elfCounter+1)
-
-# 	elfCounter += 1
-# 	if elfCounter >= len(elves): elfCounter = 0
-
-# print 'The last remaining elf with presents is: %s' % elves
-
-
-
-
-# ****************************************** challenge 2 ****************************************** 
-print '********** Challenge 2 ************'
+# ****************************************** challenge 1 ****************************************** 
+print '********** Challenge 1 ************'
 
 NoOfElves = 3005290
 
@@ -54,21 +24,54 @@ NoOfElves = 3005290
 elves = []
 for createCounter in range(NoOfElves): elves.append(createCounter+1)
 
-#print elves
 elfCounter = 0
+
 # Do the present shift and exclusion of elves until there is only one elf left
 while len(elves) > 1:
 	if len(elves) % 10000 == 0: print len(elves) / 10000
 
+	# Are we currently at the last elf in the ring, then remove the first one, otherwise just remove the next in line
+	if elfCounter == len(elves)-1:
+		elves.pop(0)
+	else:
+		elves.pop(elfCounter+1)
+
+	# Move to the next elf
+	elfCounter += 1
+	if elfCounter >= len(elves): elfCounter = 0
+
+print 'Clallenge 1: The last remaining elf with presents is: %s' % elves
+
+
+
+# ****************************************** challenge 2 ****************************************** 
+print '\n********** Challenge 2 ************'
+
+NoOfElves = 3005290
+
+
+# Create an array corresponding to the elves with one present each
+elves = []
+for createCounter in range(NoOfElves): elves.append(createCounter+1)
+
+elfCounter = 0
+
+# Do the present shift and exclusion of elves until there is only one elf left
+while len(elves) > 1:
+
 	# Calclulate the elf to be removed
-	removeElfNo = len(elves)//2 + 1 + elfCounter
-	if removeElfNo > len(elves)-1: removeElfNo -= len(elves)
+	removeElfNo = len(elves)//2 + elfCounter
+	if removeElfNo > len(elves)-1: 
+		removeElfNo -= len(elves)
+	else:
+		elfCounter += 1	
 
 	#Remove the elf
 	elves.pop(removeElfNo)
 
-	#print elves
-	elfCounter += 1
+	# Check so that our elfCounter are not out of bounds
 	if elfCounter >= len(elves): elfCounter = 0
 
-print 'The last remaining elf with presents is: %s' % elves
+	
+
+print 'Clallenge 2: The last remaining elf with presents is: %s' % elves
